ce7a776db934a6edfd93d8d1b3924c0dba6513bc
Aston Dahllöf <Aston_dahllof@outlook.com>
2023-09-06 22:42:18 -0300

    Managed to get the data extraction to work, but API key is giving me issues. Keeps giving me a 401 when I try to fetch for daily. Not sure why.

--- assets/script.js
+++ assets/script.js
@@ -11,13 +11,10 @@
 // THEN I am again presented with current and future conditions for that city
 // ```
 
+
 var searchButton = $('#search-button');
-var weatherKey = "ea99ed525bcde0ba4e8f221e94249590";
-
-// This variable holds the users input data. By default, I want it to be the users current location. I made a function above to make
-// This happen but it doesn't quite work. Will have to check why. For now, defaulting to Stockholm
-
-
+var weatherKey = "d2ef8e83e82d03b4e864eaf84dcb4676";
+//                d2ef8e83e82d03b4e864eaf84dcb4676
 
 
 // This function returns the geo pulled from apiGeo. This can be manipulated with the userLocation variable. This function
@@ -25,9 +22,10 @@ var weatherKey = "ea99ed525bcde0ba4e8f221e94249590";
 function getUserGeo(){
     var apiGeoVariable = verifyLocation();
     return fetch(apiGeoVariable).then(function (response){
-        console.log(response);
+        console.log('123123',response.status)
         return response.json();
     }).then(function (data){
+
         var cityName =data[0].name;
         var latitude =data[0].lat;
         var longitude =data[0].lon;
@@ -45,28 +43,45 @@ function getUserGeo(){
 // This function returns CITY, LAT and LON to then be parsed through openweathermap. This information then will also need to be parsed.
 async function manageData(){
     var dataArray = await getUserGeo();
-    // The above comes back undefined. FIX
     // Below variables need to be populated with items from getUserGeo.
     var inputNameData = dataArray[0];
     var latitudeData = dataArray[1];
     var longitudeData = dataArray[2];
     console.log('Returned data in manageData is: ', inputNameData, latitudeData, longitudeData)
-    // Insert function here to display name.
-    const url = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitudeData}&lon=${longitudeData}&cnt=5&appid=${weatherKey}`
-    console.log(url)
-    // Below fetches data and parses it out of const url
+     const url = "https://api.openweathermap.org/data/2.5/forecast?lat=" +latitudeData +"&lon=" +longitudeData +"&appid=" +weatherKey +"&units=metric";
     fetch(url).then(res => res.json()).then(data => {
-        console.table(data);
+        // object will return a key value pair 
+            console.log('data', data)
+        for(let i = 0; i < 5; i++){
+            var day = document.getElementById('day' + i);
+            var temp = document.getElementById('day' + i + 'temp');
+            var wind = document.getElementById('day' + i + 'wind');
+            var humidity = document.getElementById('day' + i + 'humi');
+            day.textContent = 'Date: ' + data.list[i].dt_txt
+            temp.textContent = 'Temp: ' + data.list[i].main.temp
+            wind.textContent = 'Wind: ' + data.list[i].wind.speed
+            humidity.textContent = 'Humidity: ' + data.list[i].main.humidity
+        }
     }).catch(() =>{
         console.log('Error retreving data, Error: 61');
     })
 }
 
+// const createWeatherItems = (WeatherCard) => {
+//     return `<li class="card">
+//     <p>${weatherItem.dt_txt.split("")[0]}</p>
+//     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt=weather-icon">
+//     <p>${'Temp: ' + weatherItem.temp} </p>
+//     <p>Wind: </p>
+//     <p>Humidity: </p>
+//     </li>`;
+
+// }
 
 // Add error function here. Just a simple alert. Also trim user input. NOTE TO SELF, THIS TIES INTO; getUserGeo. Watch out for scope issues.
 function verifyLocation(){
     var userLocation = document.querySelector('#searchField').value;
-    var apiGeo = `http://api.openweathermap.org/geo/1.0/direct?q=${userLocation}&limit=1&appid=${weatherKey}`;
+    var apiGeo = `https://api.openweathermap.org/geo/1.0/direct?q=${userLocation}&limit=1&appid=${weatherKey}`;
     console.log('user location is:', userLocation)
     console.log('apiGeo is: ', apiGeo)
     return apiGeo
@@ -79,31 +94,22 @@ searchButton.on('click', function () {
 })
 
 
-
 // Below gets users location, this is the default input when loading the webpage.
-
 // navigator.geolocation.getCurrentPosition(position => {const { latitude, longitude} = position.coords;
-//     const url = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&cnt=5&appid=${weatherKey}`
+//     const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&cnt=5&appid=${weatherKey}&units=metric`
 //     console.log(url)
-//     fetch(url).then(res => res.json()).then(data => {
-//         console.table(data);
+//     fetch(url).then(res => res.json()).then(dataCurrent => {
+//         console.table(dataCurrent);
+//         return dataCurrent;
 //     }).catch(() =>{
 //         console.log('Error retreving data, Error: 26');
 //     })
 // });
-// getCurrentPosition();
 
-// var searchbutton = document.getElementById('search-button');
-// var searchBox = document.getElementById('searchField');
-// var daymonday = document.getElementById('day-monday');
-// var daytuesday = document.getElementById('day-tuesday');
-// var daywednesday = document.getElementById('day-wednesday');
-// var daythursday = document.getElementById('day-thursday');
-// var dayfriday = document.getElementById('day-friday');
-// var week = [daymonday, daytuesday, daywednesday, daythursday, dayfriday]
-// for (let i = 0; i < week.length; i++) {
-//     console.log(week[i]);}
-
+// -----------------------------------------
+// below is example on how to extract data.
+// console.log(fiveDaysForecast[0].main.temp);
+// -----------------------------------------
 
 // Logic will be as follows. Upon entering a city in the searchbox, the value will be added to a variable
 // this will then be passed to the weather API for results. Once the results are returned
